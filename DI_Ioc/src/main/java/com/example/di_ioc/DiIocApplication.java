20816bee4ae52e21e2479469bf721baddcc58770
tutncrusoe <huydinhanhle@gmail.com>
2023-02-14 18:10:11 +0700

    first_DI_principle

--- DI_Ioc/src/main/java/com/example/di_ioc/DiIocApplication.java
+++ DI_Ioc/src/main/java/com/example/di_ioc/DiIocApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * Bây giờ giả sử, chúng ta định nghĩa trước toàn bộ các dependency có trong Project,
+ * mô tả nó và tống nó vào 1 cái kho và giao cho một thằng tên là framework quản lý.
+ * Bất kỳ các Class nào khi khởi tạo, nó cần dependency gì,
+ * thì cái framework này sẽ tự tìm trong kho rồi inject vào đối tượng thay chúng ta.
+ * sẽ tiện hơn phải không?
+ */
 @SpringBootApplication
 public class DiIocApplication {
 
@@ -15,6 +22,9 @@ public class DiIocApplication {
         SpringApplication.run(DiIocApplication.class, args);
     }
 
+    /**
+     * Khi đó, code chúng ta sẽ chỉ cần như này, để lấy ra 1 đối tượng:
+     */
     public BottleDI getBeanOfBottleDI() {
         return context.getBean(BottleDI.class);
     }
